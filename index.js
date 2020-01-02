58fc8a13a4cfcb3ba7a139271c09292b95b8c65f
Adedolapo Adaranijo <dolapo098@yahoo.com>
2020-01-02 07:48:47 +0100

    edit my relative path to contact

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post("/contactMe", async(req,res)=>{
 if(process.env.NODE_ENV === "production"){
     app.use(express.static( "client/build" ));
 
-    app.get("*", (req,res)=>{
+    app.get("http://localhost:2000/contactMe", (req,res)=>{
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
